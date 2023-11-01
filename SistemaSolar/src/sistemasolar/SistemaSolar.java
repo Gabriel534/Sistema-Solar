04fe8ad1eb678e73ae0200e715da0f295d6e632f
Gabriel Sampaio Santos <55637901+Gabriel534@users.noreply.github.com>
2023-11-01 15:24:06 -0300

    Add files via upload

---
+++ SistemaSolar/src/sistemasolar/SistemaSolar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistemasolar;
+import telas.Inicio;
+/**
+ *
+ * @author sp300lab
+ */
+public class SistemaSolar {
+
+    public static void main(String[] args) {
+        
+        Inicio jogo = new Inicio();
+        jogo.setVisible(true);
+    }
+}
